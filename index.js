45e720abd00924bb1c1267a9a13399e543aca973
Michael Cole <mike@S-Te-18-182.local>
2020-08-08 21:18:33 +0200

    192i30

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ client.giveawaysManager = new GiveawaysManager(client, {
         reaction: "🎉"
     }
 });
+
 client.package = require("./package.json");
 client.on("warn", console.warn); // This will warn you via logs if there was something wrong with your bot.
 client.on("error", console.error); // This will send you an error message via logs if there was something missing with your coding.
